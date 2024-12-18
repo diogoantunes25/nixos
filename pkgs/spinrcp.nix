332fb9d2563c1842113e8a5f09bc8d4bd99938ba
diogo antunes <diogo.santiago.pt@gmail.com>
2024-12-18 16:08:57 +0000

    feat: added desktop entry for SpinRCP

--- pkgs/spinrcp.nix
+++ pkgs/spinrcp.nix
@@ -1,36 +1,61 @@
 {
   pkgs,
-  lib
+    lib
 }:
-pkgs.stdenv.mkDerivation {
-  pname = "SpinRCP";
+let
+  spinPackage = pkgs.stdenv.mkDerivation {
+    pname = "SpinRCP-cli";
 
-  version = "3.11";
-  src = fetchTarball {
-    url = "http://lms.uni-mb.si/spinrcp/releases/SpinRCP311-linux64.zip";
-    sha256 = "13d2m13nkymd6d39h94sxc2n9jh2nkjhmcg5q64b0xiay58x68sr";
+    version = "3.11";
+    src = fetchTarball {
+      url = "http://lms.uni-mb.si/spinrcp/releases/SpinRCP311-linux64.zip";
+      sha256 = "13d2m13nkymd6d39h94sxc2n9jh2nkjhmcg5q64b0xiay58x68sr";
+    };
+
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook
+        pkgs.makeWrapper
+    ];
+
+    buildInputs = [ pkgs.spin pkgs.jdk8 pkgs.swt pkgs.xorg.libXtst ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r . $out/bin
+      cd $out/bin
+
+      echo "-vm" > SpinRCP.ini
+      echo "${pkgs.jdk8}/bin/java" > SpinRCP.ini
+
+      chmod u+x ./SpinRCP
+      '';
+
+    postFixup = ''
+      mv $out/bin/SpinRCP $out/bin/SpinRCPUnwrapped
+      makeWrapper $out/bin/SpinRCPUnwrapped $out/bin/SpinRCP --set PATH ${lib.makeBinPath [ pkgs.jdk8 pkgs.xorg.libXtst ]} --set LD_LIBRARY_PATH ${pkgs.xorg.libXtst}/lib
+      '';
   };
 
-  nativeBuildInputs = [
-    pkgs.autoPatchelfHook
-    pkgs.makeWrapper
-  ];
-
-  buildInputs = [ pkgs.spin pkgs.jdk8 pkgs.swt pkgs.xorg.libXtst ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r . $out/bin
-    cd $out/bin
-
-    echo "-vm" > SpinRCP.ini
-    echo "${pkgs.jdk8}/bin/java" > SpinRCP.ini
-
-    chmod u+x ./SpinRCP
-    '';
-
-  postFixup = ''
-    mv $out/bin/SpinRCP $out/bin/SpinRCPUnwrapped
-    makeWrapper $out/bin/SpinRCPUnwrapped $out/bin/SpinRCP --set PATH ${lib.makeBinPath [ pkgs.jdk8 pkgs.xorg.libXtst ]} --set LD_LIBRARY_PATH ${pkgs.xorg.libXtst}/lib
-  '';
+  desktop = pkgs.makeDesktopItem {
+    name = "SpinRCP-application";
+    desktopName = "SpinRCP";
+    genericName = "SpinRCP";
+    exec = ''${spinPackage}/bin/SpinRCP'';
+    # categories = [ "Network" ];
+  };
+in
+# Combined
+pkgs.symlinkJoin {
+  name = "SpinRCP"; paths = [ spinPackage desktop ];
 }
+# pkgs.stdenv.mkDerivation {
+#   pname = "SpinRCP";
+#   version = spinPackage.version;
+#   buildInputs = [ spinPackage desktop ];
+#
+#   installPhase = ''
+#       mkdir -p $out/bin $out/share/applications
+#       ln -s ${spinPackage}/bin/SpinRCP $out/bin/SpinRCP
+#       cp ${desktop}/share/applications/*.desktop $out/share/applications/
+#     '';
+# }
